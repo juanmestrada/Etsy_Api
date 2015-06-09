bc1311ed9fd24b3eb42ba401de7c904046ca847a
Juan Estrada <juanestrada@Juans-MBP.Home>
2015-06-09 00:55:17 -0500

    third commit

--- dist/app-browserify.js
+++ dist/app-browserify.js
@@ -1,1 +1,59 @@
+"use strict";
 
+// es5 polyfills, powered by es5-shim
+require("es5-shim")
+// es6 polyfills, powered by babel
+require("babel/register")
+
+var Promise = require('es6-promise').Promise
+var $ = require('jquery')
+var Backbone = require('backbone')
+
+
+import * as templates from "./templates.js"
+
+import * as api from './etsy-api.js'
+
+
+
+var EtsyRouter = Backbone.Router.extend({
+	routes: {
+		'listings/:id': 'details',
+		'search/:keywords': 'search',
+		'*default':'home'
+	},
+	home: function(anything){
+
+		api.getListings().then((etsyListings_json)=> {
+			console.log(etsyListings_json)
+
+			document.body.innerHTML= templates.home(etsyListings_json.results)
+		})
+	},
+	details:function(id){
+		api.getListing(id).then((etsyListing_json)=>{
+			var v = etsyListing_json.results[0]
+		document.body.innerHTML = templates.details(v) 
+	})
+},
+search: function(keywords) {
+	api.getItems(keywords).then((keywords_json)=> {
+
+	document.body.innerHTML=templates.home(keywords_json.results)
+})
+
+},
+	initialize:function(){
+		Backbone.history.start()
+
+	},
+})
+
+var router = new EtsyRouter()
+
+// if (form_search) {
+$('body').on('submit', 'form.search_form', (event) => {
+    event.preventDefault();
+	window.location.hash = `search/${document.querySelector(".search_item").value}`
+})
+// }
---
+++ js/templates.js
@@ -0,0 +1,318 @@
+var toolbar = function() {
+    return '<header id="top">
+        <div class="container_nav">
+            <div class="logo_nav" >
+              <a class="log "href="https://medium.com/@jestradaf22">Etsy</a>
+            </div>
+
+            <div class="search_nav">   
+               <form id="searchbox_nav" action="">
+                  <input id="search" type="text" placeholder="Search For Items">
+                  <input id="submit" type="submit" value="Search">
+             </form>
+            </div>
+
+        </div>
+    </header>'
+}
+
+var grid_cell = (url, title) => '<div class="grid_cell">
+
+                        <div class="cell_info">
+                            <div id="etsy_item" >
+                                <a href="#">
+                                    <img src="${url}">
+                                </a>
+                            </div>
+                            <div class="etsy_details_box">
+                                <div class="etsy_details">
+                                    <p/>test item details</p>
+                                </div>
+                                <div class="etsy_details_seller">
+                                    <p>Test Seller<span class="etsy_seller_price">Test</span><p>
+                                </div>
+                            </div>
+                        </div>  
+                    </div>'
+
+export var details = (url, title) => '${toolbar()}
+   <div class="details_content">
+        <div class="details_body">
+            <div class="listing">
+                <div class="listing_left">
+                    <div id="image_wrapper">
+                        <div id="main_image">
+                            <img src=""${url}"">
+                        </div>
+                        <div id="other_images"> 
+                            <ul id="image_list">
+                                <li id="image-0" image-index="0">
+                                    <img src="#">
+                                </li>
+                                <li id="image-1" image-index="1">
+                                    <img src="#">
+                                </li>
+                                <li id="image-2" image-index="2">
+                                    <img src="#">
+                                </li>
+                                <li id="image-3" image-index="3">
+                                    <img src="#">
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="listing_description">
+                        <div class="description_text"><br>Test<br><br>Test<br><br>Test<br><br><br>Test</div>
+                    </div>
+                </div>
+                <div class="listing_right">
+                    <div class="listing_cart_area">
+                        <div class="listing_overview">
+                            <div id="listing_right_text">
+                                <h1><span id="lisitng_right_title">Test</span></h1>
+                            </div>
+                            <div class="listing_right_overview">
+                                <h3>Overview</h3>
+                                <ul classs="listing_properties">
+                                    <li>Handmade Item</li>
+                                    <li>Materials:<span id="listing_materials"></span</li>
+                                    <li>FeedBack:<span id="listing_feedback"></span></li>
+                                    <li></li>
+                                    <li></li>
+                                </ul>
+                            </div>
+                        </div>  
+                    </div>  
+                    <div class="related_listings_wrapper">
+                        <div class="related_listings">
+                            <div id="seller_info">
+                                <div id="avatar"></div>
+                                <div id="shop_name"></div>
+                            </div>
+                            <div id="other_items">
+                                <ul>
+                                    <div class="other_items_cell">
+                                        <div  class="other_items_image">
+                                        <a  href="#">
+                                            <img src=""${url}"">
+                                        </a>
+                                        </div>
+                                        <div class="other_items_cell_info">
+                                            <div id="other_items_cell_details">Test</div>
+                                            <div id="other_items_cell_price">$test</div>
+                                        </div>  
+                                    </div>
+                                    <div class="other_items_cell">
+                                        <div  class="other_items_image">
+                                        <a  href="#">
+                                            <img src=""${url}"">
+                                        </a>
+                                        </div>
+                                        <div class="other_items_cell_info">
+                                            <div id="other_items_cell_details">Test</div>
+                                            <div id="other_items_cell_price">$test</div>
+                                        </div>  
+                                    </div>
+                                    <div class="other_items_cell">
+                                        <div  class="other_items_image">
+                                        <a  href="#">
+                                            <img src=""${url}"">
+                                        </a>
+                                        </div>
+                                        <div class="other_items_cell_info">
+                                            <div id="other_items_cell_details">Test</div>
+                                            <div id="other_items_cell_price">$test</div>
+                                        </div>  
+                                    </div>
+                                    <div class="other_items_cell">
+                                        <div  class="other_items_image">
+                                        <a  href="#">
+                                            <img src=""${url}"">
+                                        </a>
+                                        </div>
+                                        <div class="other_items_cell_info">
+                                            <div id="other_items_cell_details">Test</div>
+                                            <div id="other_items_cell_price">$test</div>
+                                        </div>  
+                                    </div>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>      
+    </div>'
+
+
+
+
+
+export var home = (Etsy) => '${toolbar()}
+    <div class="content">
+            <div class="aside_all">
+            <h5>Show Results For:</h5>
+                <div class="aside_outer">
+                    <div class="aside_inner">
+                        <ul>
+                            <li>
+                                <a class="aside_links" href="#"><span class="aside_text">Beer</span>
+                            </li>
+                            <li>
+                                <a class="aside_links" href="#"><span class="aside_text">Sports</span>
+                            </li>
+                            <li>
+                                <a class="aside_links" href="#"><span class="aside_text">Cars</span>
+                            </li>
+                            <li>
+                                <a class="aside_links" href="#"><span class="aside_text">Guns</span>
+                            </li>
+                            <li>
+                                <a class="aside_links" href="#"><span class="aside_text">Grilling Stuff</span>
+                            </li>
+                            <li>
+                                <a class="aside_links" href="#"><span class="aside_text">Tools</span>
+                            </li>
+                            <li>
+                                <a class="aside_links" href="#"><span class="aside_text">Beards</span>
+                            </li>
+                            <li>
+                                <a class="aside_links" href="#"><span class="aside_text">Beard Accessories</span>
+                            </li>
+                            <li>
+                                <a class="aside_links" href="#"><span class="aside_text">Bare Knuckle Boxing</span>
+                            </li>
+                            <li>
+                                <a class="aside_links" href="#"><span class="aside_text">Explosions</span>
+                            </li>
+                            <li>
+                                <a class="aside_links" href="#"><span class="aside_text">More Manly Things</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+           <div class="grid">
+                <div class="grid_info">
+                    <div class="grid_left">
+                        <span>
+                            <a class="grid_info_link" href="#" title="All Categories"><span class="grid_info_etzy">All Categories</span></a>
+                        </span>
+                        <span class="grid_info_api">Test</span>
+                        
+                    </div>
+                    <div class="grid_right"><span class="grid_info_common">Sort By:<span class="grid_info_etzy">Revelancy</span></span></div>
+                </div>
+                <div class="grid_inner">
+                    <div class="grid_cell">
+
+                        <div class="cell_info">
+                            <div id="etsy_item" >
+                                <a href="#">
+                                    <img src="${url}">
+                                </a>
+                            </div>
+                            <div class="etsy_details_box">
+                                <div class="etsy_details">
+                                    <p/>test item details</p>
+                                </div>
+                                <div class="etsy_details_seller">
+                                    <p>Test Seller<span class="etsy_seller_price">Test</span><p>
+                                </div>
+                            </div>
+                        </div>  
+                    </div>
+                    <div class="grid_cell">
+
+                        <div class="cell_info">
+                            <div id="etsy_item" >
+                                <a href="#">
+                                    <img src="${url}">
+                                </a>
+                            </div>
+                            <div class="etsy_details_box">
+                                <div class="etsy_details">
+                                    <p/>test item details</p>
+                                </div>
+                                <div class="etsy_details_seller">
+                                    <p>Test Seller<span class="etsy_seller_price">Test</span><p>
+                                </div>
+                            </div>
+                        </div>  
+                    </div>
+                    <div class="grid_cell">
+
+                        <div class="cell_info">
+                            <div id="etsy_item" >
+                                <a href="#">
+                                    <img src="${url}">
+                                </a>
+                            </div>
+                            <div class="etsy_details_box">
+                                <div class="etsy_details">
+                                    <p/>test item details</p>
+                                </div>
+                                <div class="etsy_details_seller">
+                                    <p>Test Seller<span class="etsy_seller_price">Test</span><p>
+                                </div>
+                            </div>
+                        </div>  
+                    </div>
+                    <div class="grid_cell">
+
+                        <div class="cell_info">
+                            <div id="etsy_item" >
+                                <a href="#">
+                                    <img src="${url}">
+                                </a>
+                            </div>
+                            <div class="etsy_details_box">
+                                <div class="etsy_details">
+                                    <p/>test item details</p>
+                                </div>
+                                <div class="etsy_details_seller">
+                                    <p>Test Seller<span class="etsy_seller_price">Test</span><p>
+                                </div>
+                            </div>
+                        </div>  
+                    </div>
+                    <div class="grid_cell">
+
+                        <div class="cell_info">
+                            <div id="etsy_item" >
+                                <a href="#">
+                                    <img src="${url}">
+                                </a>
+                            </div>
+                            <div class="etsy_details_box">
+                                <div class="etsy_details">
+                                    <p/>test item details</p>
+                                </div>
+                                <div class="etsy_details_seller">
+                                    <p>Test Seller<span class="etsy_seller_price">Test</span><p>
+                                </div>
+                            </div>
+                        </div>  
+                    </div>
+                    <div class="grid_cell">
+
+                        <div class="cell_info">
+                            <div id="etsy_item" >
+                                <a href="#">
+                                    <img src="${url}">
+                                </a>
+                            </div>
+                            <div class="etsy_details_box">
+                                <div class="etsy_details">
+                                    <p/>test item details</p>
+                                </div>
+                                <div class="etsy_details_seller">
+                                    <p>Test Seller<span class="etsy_seller_price">Test</span><p>
+                                </div>
+                            </div>
+                        </div>  
+                    </div>
+                </div>  
+            </div>
+        
+    </div>`
